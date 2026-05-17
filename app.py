8f6a9df4a46d4b76d83194dacc8c3c4134f7ed2e
Bekiaris Dimitrios <dbekiaris@xo.gr>
2026-05-17 12:52:07 +0300

    Refactor api folder.

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 # Εκκινεί την εφαρμογή
 from flask import Flask
 
-from api.routes.Comments import create_comment, get_comments
-from api.routes.States import create_state, delete_state, get_state, get_states, update_state
-from api.routes.Categories import create_category, delete_category, get_categories, get_category, update_category
-from api.routes.Problems import create_problem, delete_problem, get_problem, get_problems, update_problem
+from api.routes.all import define_routes
 
 app = Flask(__name__)
 
@@ -12,25 +9,7 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-app.add_url_rule('/states', methods = ['GET'], view_func = get_states)
-app.add_url_rule('/states/<string:id>', methods = ['GET'], view_func = get_state)
-app.add_url_rule('/states', methods = ['POST'], view_func = create_state)
-app.add_url_rule('/states/<string:id>', methods = ['PUT'], view_func = update_state)
-app.add_url_rule('/states/<string:id>', methods = ['DELETE'], view_func = delete_state)
-
-app.add_url_rule('/categories', methods = ['GET'], view_func = get_categories)
-app.add_url_rule('/categories/<string:id>', methods = ['GET'], view_func = get_category)
-app.add_url_rule('/categories', methods = ['POST'], view_func = create_category)
-app.add_url_rule('/categories/<string:id>', methods = ['PUT'], view_func = update_category)
-app.add_url_rule('/categories/<string:id>', methods = ['DELETE'], view_func = delete_category)
-
-app.add_url_rule('/problems', methods = ['GET'], view_func = get_problems)
-app.add_url_rule('/problems/<string:id>', methods = ['GET'], view_func = get_problem)
-app.add_url_rule('/problems', methods = ['POST'], view_func = create_problem)
-app.add_url_rule('/problems/<string:id>', methods = ['PUT'], view_func = update_problem)
-app.add_url_rule('/problems/<string:id>', methods = ['DELETE'], view_func = delete_problem)
-app.add_url_rule('/problems/<string:problemId>/comments', methods = ['GET'], view_func = get_comments)
-app.add_url_rule('/problems/<string:problemId>/comments', methods = ['POST'], view_func = create_comment)
+define_routes(app)
 
 if __name__ == '__main__':
     app.run(debug=False)
