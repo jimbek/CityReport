46efc3555f8bbc2a163c89ee25e7abe6ae3f5750
Georgoulias Alekos <georgouliasalekos@gmail.com>
2026-05-24 14:41:58 +0300

    Updated basic folders

--- admin.py
+++ admin.py
@@ -2,7 +2,11 @@
 # Pages are defined in the pages/ directory and imported here.
 import streamlit as st
 
-index = st.Page("pages/index.py", title = "Αναζήτηση",icon = ":material/search:")
+from lib.repos.DatabaseInitializer import initialize_database
+
+initialize_database()
+
+index = st.Page("pages/index.py", title = "Προβλήματα", visibility = "hidden")
 
 per_category = st.Page("pages/statistics/per_category.py", title = "Στατιστικά ανά κατηγορία",icon = ":material/bar_chart:")
 per_state = st.Page("pages/statistics/per_state.py", title = "Στατιστικά ανά κατάσταση",icon = ":material/bar_chart:")
