a36e9ffb7d41c5f53e72c909d11ba7215b72b5f3
Georgoulias Alekos <georgouliasalekos@gmail.com>
2026-05-24 14:57:57 +0300

    Add error handling to comment REST endpoints

--- routes/Comments.py
+++ routes/Comments.py
@@ -13,33 +13,39 @@ problem_repo = ProblemsRepository()
 
 # GET /problems/<string:problemId>/comments - Get comments for a problem.
 def get_comments(problemId: str):
-    if problemId is None or problemId == '':
-        return "Missing problemId", 400
+    try:
+        if problemId is None or problemId == '':
+            return "Missing problemId", 400
 
-    if problem_repo.problem_exists(problemId) is False:
-        return "Problem not found", 404
+        if problem_repo.problem_exists(problemId) is False:
+            return "Problem not found", 404
 
-    return repo.get_comments(problemId), 200
+        return repo.get_comments(problemId), 200
+    except Exception as error:
+        return f"Internal server error: {error}", 500
 
 # POST /problems/<string:problemId>/comments - Create a comment for a problem.
 def create_comment(problemId: str):
-    if problemId is None or problemId == '':
-        return "Missing problemId", 400
-    
-    data = request.get_json()
+    try:
+        if problemId is None or problemId == '':
+            return "Missing problemId", 400
+        
+        data = request.get_json()
 
-    if data is None:
-        return "Body is missing", 400
-    
-    if not data.get("author") or not data.get("content"):
-        return "Missing parameters", 400
-    
-    if "author" in data and data["author"] not in ["Πολίτης", "Διαχειριστής"]:
-        return "Invalid author", 400
-    
-    if problem_repo.problem_exists(problemId) is False:
-        return "Problem not found", 404
-    
-    data['problemId'] = problemId
+        if data is None:
+            return "Body is missing", 400
+        
+        if not data.get("author") or not data.get("content"):
+            return "Missing parameters", 400
+        
+        if "author" in data and data["author"] not in ["Πολίτης", "Διαχειριστής"]:
+            return "Invalid author", 400
+        
+        if problem_repo.problem_exists(problemId) is False:
+            return "Problem not found", 404
+        
+        data['problemId'] = problemId
 
-    return repo.add_comment(data), 201
+        return repo.add_comment(data), 201
+    except Exception as error:
+        return f"Internal server error: {error}", 500
